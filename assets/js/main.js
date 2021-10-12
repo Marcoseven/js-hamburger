e7157fb0c9e0125b9e45fdac1e265903e43c9fd5
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-12 17:40:09 +0200

    Aggiunte modifiche
    Inserite tre variabili const, i vari commenti e le due function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+/* dobbiamo mostrare/nascondere menu hamburger */
 
+// variabili const
+const openHamburger = document.querySelector(".header-right>a");
+const closeHamburger = document.querySelector(".hamburger-menu>a");
+const hamburger = document.querySelector(".hamburger-menu");
+
+// apertura e chiusura .hamburger-menu
+openHamburger.addEventListener("click", function (addActiveClass) {
+	hamburger.classList.add("active");
+});
+
+closeHamburger.addEventListener("click", function (addActiveClass) {
+	hamburger.classList.remove("active");
+});
